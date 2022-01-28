4211257383e42db8f78119d5d105ac4ffe378499
Emanuel Acosta <emanuelacosta809@gmail.com>
2022-01-28 15:01:35 -0400

    added end scene

---
+++ Assets/Scripts/MagufinCode.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagufinCode : MonoBehaviour
+{
+    public static int magufinColected = 0;
+    readonly int MaxMagufinsColcted = 6;
+    string sceneToLoadWhenGameIsDone = "End";
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            magufinColected++;
+            if (MaxMagufinsColcted == magufinColected) {
+                SceneLoader.LoadScene("end", "exit1");
+            }
+            Destroy(gameObject);
+        }
+    }
+}
