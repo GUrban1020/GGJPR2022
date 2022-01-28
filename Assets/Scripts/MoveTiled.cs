48e72f080510fcf53b63cc0111c6091ed1846b34
Emanuel Acosta <emanuelacosta809@gmail.com>
2022-01-28 18:09:57 -0400

    camera mov

--- Assets/Scripts/MoveTiled.cs
+++ Assets/Scripts/MoveTiled.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveTiled : MonoBehaviour
 {
     public Vector3 screenSize;
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,13 @@ public class MoveTiled : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 p = transform.position; 
+        Vector3 p = target.position;
+        for (int i = 0; i < 3; i++)
+        {
+            p[i] = Mathf.Round(p[i] / screenSize[i]) * screenSize[i];
+        }
+        p.z = -10;
+        transform.position = p;
+       // transform.position = Grid.Swizzle(GridLayout.CellSwizzle.XYZ,p);
     }
 }
