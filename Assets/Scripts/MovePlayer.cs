74e6d0851e792099adef16476438b45f6309c196
Emanuel Acosta <emanuelacosta809@gmail.com>
2022-01-29 12:02:29 -0400

    added animations

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -7,22 +7,26 @@ public class MovePlayer : MonoBehaviour
     public float speedUnitsPerSeconds = 3;
     public float lookX;
     public float lookY;
-    public Animator animator;
+    public bool IsMoving;
+    public Animator[] animators;
   
     void FixedUpdate()
     {
         Vector3 input = new Vector3 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (input.x != 0)
-            lookX = Mathf.Sign(input.x);
+       // if (input.x != 0)
+           // lookX = Mathf.Sign(input.x);
         
-        if (input.y != 0)
-            lookX = Mathf.Sign(input.y);
+        //if (input.y != 0)
+            //lookX = Mathf.Sign(input.y);
 
-        if (animator)
+        IsMoving = (input != Vector3.zero);
+        foreach (var animator in animators)
         {
-           // animator.SetFloat();
-           // animator.SetFloat();
+            animator.SetFloat("lookY", input.y);
+            animator.SetBool("isMoving", IsMoving);
+            animator.SetFloat("lookX", input.x);
         }
+        
 
         transform.position += input * speedUnitsPerSeconds * Time.fixedDeltaTime;
     }
